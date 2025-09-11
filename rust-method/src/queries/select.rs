ac51256887bed175e4af0d74e592da12072353cd
TrifanBogdan24 <trifanbogdan24@gmail.com>
2025-09-11 11:18:47 +0300

    [Rust - Task 2] completed SELECT ; generic impls ; project modularization

---
+++ rust-method/src/queries/select.rs
@@ -0,0 +1,168 @@
+use crate::Secretariat;
+use crate::Student;
+use crate::Materie;
+use crate::Inrolare;
+
+use crate::queries::where_clause::*;
+
+
+// Trait generic
+trait Selectable {
+    fn print_table_field(&self, field: &str) -> Result<(), String>;
+}
+
+impl Selectable for Student {
+    fn print_table_field(&self, field: &str) -> Result<(), String> {
+        match field {
+            "id" => print!("{}", self.id),
+            "nume" => print!("{}", self.nume),
+            "an_studiu" => print!("{}", self.an_studiu),
+            "statut" => print!("{}", self.statut),
+            "medie_generala" => print!("{:.2}", self.medie_generala),
+            _ => return Err(format!("Camp invalid {:?}", field)),
+        }
+        Ok(())
+    }
+}
+
+impl Selectable for Materie {
+    fn print_table_field(&self, field: &str) -> Result<(), String> {
+        match field {
+            "id" => print!("{}", self.id),
+            "nume" => print!("{}", self.nume),
+            "nume_titular" => print!("{}", self.nume_titular),
+            _ => return Err(format!("Camp invalid {:?}", field)),
+        }
+        Ok(())
+    }
+}
+
+
+impl Selectable for Inrolare {
+    fn print_table_field(&self, field: &str) -> Result<(), String> {
+        match field {
+            "id_student" => print!("{}", self.id_student),
+            "id_materie" => print!("{}", self.id_materie),
+            "note" => print!("{:.2} {:.2} {:.2}", 
+                             self.note[0], self.note[1], self.note[2]),
+            _ => return Err(format!("Camp invalid {:?}", field)),
+        }
+        Ok(())
+    }
+}
+
+// Functie generica
+fn select_from_table<T: Selectable + Matchable>(
+    entries: &[T],
+    campuri: &[&str],
+    conditii: &Vec<Conditie>,
+) -> Result<(), String> {
+    for entry in entries {
+        match match_on_all_conditii(entry, conditii) {
+            Ok(true) => (),
+            Ok(false) => continue,
+            Err(message) => return Err(message),
+        }
+
+        for (idx, field) in campuri.iter().enumerate() {
+            entry.print_table_field(field)?;
+            if idx != campuri.len() - 1 {
+                print!(" ");
+            }
+        }
+
+        println!();
+    }
+
+    Ok(())
+}
+
+
+
+
+
+
+/* Template:
+SELECT * FROM <tabel>;
+SELECT <campuri> FROM <tabel>;
+SELECT <campuri> FROM <tabel> WHERE <camp> <operator> <valoare>;
+SELECT <campuri> FROM <tabel> WHERE <cond1> AND <cond2>;
+
+Campuri - unul sau mai multe nume de coloane, despartite prin virgula ','
+*/
+pub fn select(s: &Secretariat, query: &str) -> Result<(), String> {
+    let select_pos = query.find("SELECT")
+        .ok_or_else(|| "SELECT nu a fost gasit in query".to_string())?;
+    
+    let mut ptr = &query[select_pos + "SELECT".len()..];
+    ptr = ptr.trim_start();
+
+    let from_pos = ptr.find("FROM")
+        .ok_or_else(|| "FROM nu a fost gasit dupa SELECT".to_string())?;
+
+    // Extrage <campuri>
+    let str_campuri: &str = ptr[..from_pos].trim();
+    let is_select_all: bool = str_campuri == "*";
+
+    // Extrage <nume_tabela>
+    ptr = &ptr[from_pos + "FROM".len()..];
+    ptr = ptr.trim_start();
+
+    let where_pos = ptr.find("WHERE");
+    let nume_tabela = if let Some(pos) = where_pos {
+        ptr[..pos].trim()
+    } else {
+        // pana la ';'
+        if let Some(end) = ptr.find(';') {
+            ptr[..end].trim()
+        } else {
+            ptr.trim()
+        }
+    };
+
+    // Extrage <conditii>, daca exista WHERE
+    let mut str_conditii = "";
+    if let Some(pos) = where_pos {
+        ptr = &ptr[pos + "WHERE".len()..];
+        ptr = ptr.trim_start();
+        if let Some(end) = ptr.find(';') {
+            str_conditii = ptr[..end].trim();
+        } else {
+            str_conditii = ptr.trim();
+        }
+    }
+
+    let mut campuri: Vec<&str>;
+    if is_select_all {
+        campuri = match nume_tabela {
+            "studenti" => vec!["id", "nume", "an_studiu", "statut", "medie_generala"],
+            "materii" => vec!["id", "nume", "nume_titular"],
+            "inrolari" => vec!["id_student", "id_materie", "note"],
+            _ =>  return Err(format!(
+                "Tabela {:?} nu exista in baza de date a facultati!",
+                nume_tabela))
+        }
+    } else {
+        // Imarte campurile de ","
+        campuri = str_campuri
+            .split(',')
+            .map(|s| s.trim())
+            .collect();
+    }
+
+    let conditii: Vec<Conditie> = if let Ok(array) = parseaza_conditiile_where(str_conditii) {
+        array
+    } else {
+        return Err("Eroare la parsarea conditiilor!".to_string());
+    };
+
+    match nume_tabela {
+        "studenti" => select_from_table(&s.studenti, &campuri, &conditii),
+        "materii"  => select_from_table(&s.materii, &campuri, &conditii),
+        "inrolari" => select_from_table(&s.inrolari, &campuri, &conditii),
+        _ => Err(format!(
+            "Tabela {:?} nu exista in baza de date a facultati!",
+            nume_tabela
+        )),
+    }
+}
---
+++ rust-method/src/queries/where_clause.rs
@@ -0,0 +1,226 @@
+use crate::Student;
+use crate::Materie;
+use crate::Inrolare;
+
+use std::str::FromStr;
+
+#[derive(Debug)]
+pub struct Conditie {
+    pub camp: String,
+    pub op_comp: String,
+    pub valoare: String
+}
+
+// Trait general
+pub trait Matchable {
+    fn matches_condition(&self, cond: &Conditie) -> Result<bool, String>;
+}
+
+// Implementări pentru tipurile tale
+impl Matchable for Student {
+    fn matches_condition(&self, cond: &Conditie) -> Result<bool, String> {
+        match_student_on_conditie(self, cond)
+    }
+}
+
+impl Matchable for Materie {
+    fn matches_condition(&self, cond: &Conditie) -> Result<bool, String> {
+        match_materie_on_conditie(self, cond)
+    }
+}
+
+impl Matchable for Inrolare {
+    fn matches_condition(&self, cond: &Conditie) -> Result<bool, String> {
+        match_inrolare_on_conditie(self, cond)
+    }
+}
+
+
+pub fn match_on_all_conditii<T: Matchable>(item: &T, conditii: &[Conditie]) -> Result<bool, String> {
+    for cond in conditii {
+        match item.matches_condition(cond) {
+            Ok(true) => continue,
+            Ok(false) => return Ok(false),
+            Err(message) => return Err(message),
+        }
+    }
+    Ok(true)
+}
+
+
+pub fn parseaza_conditiile_where(str_conditii: &str) -> Result<Vec<Conditie>, String> {
+    let mut conditii: Vec<Conditie> = Vec::new();
+
+    // Mai intai spargem dupa "AND"
+    let conditii_strings: Vec<&str> = str_conditii
+        .split("AND")
+        .map(|s| s.trim()) // elimina spatiile la inceput si sfarsit
+        .filter(|s| !s.is_empty()) // ignore empty tokens
+        .collect();
+
+    for cond_str in conditii_strings {
+        // Fiecare conditie trebuie sa aiba cel putin 3 token-uri: camp, operator, valoare
+        let mut tokens = cond_str.splitn(3, ' '); // split in maxim 3 parti
+
+        let camp = tokens.next().ok_or(format!("Camp lipsa in conditie: '{}'", cond_str))?.trim();
+        let op_comp = tokens.next().ok_or(format!("Operator lipsa in conditie: '{}'", cond_str))?.trim();
+        let valoare = tokens.next().ok_or(format!("Valoare lipsa in conditie: '{}'", cond_str))?.trim();
+
+        // Elimina ghilimelele de la inceput si sfarsit
+        let valoare = valoare.trim_matches('"').to_string();
+
+        conditii.push(Conditie {
+            camp: camp.to_string(),
+            op_comp: op_comp.to_string(),
+            valoare,
+        });
+    }
+
+    Ok(conditii)
+}
+
+
+fn match_num_on_conditie<T>(num: T, cond: &Conditie) -> Result<bool, String>
+where
+    T: FromStr + PartialEq + PartialOrd + std::fmt::Debug,
+{
+    let mut valoare: T;
+    if let Ok(num) = cond.valoare.parse() {
+        valoare = num;
+    } else {
+        return Err(format!("Valoarea furnizata pentru parametrul <num> nu este un numar: {:?}", num));
+    }
+
+    match cond.op_comp.as_str() {
+        "=" =>
+            Ok(num == valoare),
+        "!=" =>
+            Ok(num != valoare),
+        "<" =>
+            Ok(num < valoare),
+        ">" =>
+            Ok(num > valoare),
+        "<=" =>
+            Ok(num <= valoare),
+        ">=" =>
+            Ok(num >= valoare),
+        _ => Err(format!("Operator invalid de comparatie (numerica) {:?}", cond.op_comp))
+    }
+}
+
+
+fn match_student_on_conditie(student: &Student, cond: &Conditie) -> Result<bool, String> {
+    match cond.camp.as_str() {
+        "id" =>
+            return match_num_on_conditie(student.id, cond),
+        "nume" => {
+            match cond.op_comp.as_str() {
+                "=" =>
+                    return Ok(student.nume == cond.valoare),
+                "!=" =>
+                    return Ok(student.nume != cond.valoare),
+            _ => return Err(format!(
+                "Operator invalid de comparatie pentru campul \"nume\": {:?}",
+                cond.op_comp))
+            }
+        }
+        "an_studiu" =>
+            return match_num_on_conditie(student.an_studiu, cond),
+        "statut" => {
+            match cond.valoare.as_str() {
+                "t" =>
+                    return Ok(student.statut == 't'),
+                "b" =>
+                    return Ok(student.statut == 'b'),
+                _ => return Err(format!(
+                    "Valoarea de comparatie {:?} este invalida pentru statutul unui student",
+                    cond.valoare))
+            }
+        }
+        "medie_generala" =>
+            return match_num_on_conditie(student.medie_generala, cond),
+        _ => return Err(format!("Campul {:?} este invalid pentru tabela \"studenti\"", cond.camp))
+    }
+}
+
+fn match_materie_on_conditie(materie: &Materie, cond: &Conditie) -> Result<bool, String> {
+    match cond.camp.as_str() {
+        "id" =>
+            return match_num_on_conditie(materie.id, cond),
+        "nume" => {
+            match cond.op_comp.as_str() {
+                "=" =>
+                    return Ok(materie.nume == cond.valoare),
+                "!=" =>
+                    return Ok(materie.nume != cond.valoare),
+            _ => return Err(format!(
+                "Operator invalid de comparatie pentru campul \"nume\": {:?}",
+                cond.op_comp))
+            }
+        }
+        "nume_titular" => {
+            match cond.op_comp.as_str() {
+                "=" =>
+                    return Ok(materie.nume_titular == cond.valoare),
+                "!=" =>
+                    return Ok(materie.nume_titular != cond.valoare),
+            _ => return Err(format!(
+                "Operator invalid de comparatie pentru campul \"nume_titular\": {:?}",
+                cond.op_comp))
+            }
+        }
+        _ => return Err(format!("Campul {:?} este invalid pentru tabela \"materii\"", cond.camp))
+    }
+}
+
+
+
+
+fn match_inrolare_on_conditie(inrolare: &Inrolare, cond: &Conditie) -> Result<bool, String> {
+    match cond.camp.as_str() {
+        "id_student" =>
+            return match_num_on_conditie(inrolare.id_student, cond),
+        "id_materie" =>
+            return match_num_on_conditie(inrolare.id_student, cond),
+        "note" => {
+            let mut tokens = cond.valoare.split_whitespace();
+
+            let nota_laborator: f32 = tokens
+                .next()
+                .ok_or("Lipseste nota laborator")?
+                .parse()
+                .map_err(|_| "Nota laborator invalida")?;
+
+            let nota_partial: f32 = tokens
+                .next()
+                .ok_or("Lipseste nota partial")?
+                .parse()
+                .map_err(|_| "Nota partial invalida")?;
+
+            let nota_final: f32 = tokens
+                .next()
+                .ok_or("Lipseste nota final")?
+                .parse()
+                .map_err(|_| "Nota final invalida")?;
+
+            match cond.op_comp.as_str() {
+                "=" =>
+                    return Ok(
+                        nota_laborator == inrolare.note[0]
+                        && nota_partial == inrolare.note[1]
+                        && nota_final == inrolare.note[2]
+                    ),
+                "!=" =>
+                    return Ok(
+                            nota_laborator != inrolare.note[0]
+                            || nota_partial != inrolare.note[1]
+                            || nota_final != inrolare.note[2]
+                        ),
+                _ => return Err(format!(
+                    "Operator invalid de comparatie pentru campul \"note\": {:?}",
+                    cond.op_comp))
+                }
+            }
+        _ => return Err(format!("Campul {:?} este invalid pentru tabela \"inrolari\"", cond.camp))
+    }
+}
